d1617e5dd7844598e65beba6c9139125b1b04a09
Huy Ha <huyhnc81@gmail.com>
2020-03-06 01:45:38 +0700

    update filter condition

--- ImageSlicer/MainWindow.xaml.cs
+++ ImageSlicer/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace ImageSlicer
             public int Status { get; set; }
             public int LabelNum { get; set; }
             public Pic Value { get; set; }
-            public Label(int num ,int status, Pic value)
+            public Label(int num, int status, Pic value)
             {
                 LabelNum = num;
                 Status = status;
@@ -37,7 +37,7 @@ namespace ImageSlicer
             public int highX { get; set; }
             public int lowY { get; set; }
             public int highY { get; set; }
-            public Pic(int lX,int hX,int lY,int hY)
+            public Pic(int lX, int hX, int lY, int hY)
             {
                 lowX = lX;
                 highX = hX;
@@ -63,7 +63,7 @@ namespace ImageSlicer
             }
         }
 
-        
+
         private void SliceButton_Click(object sender, RoutedEventArgs e)
         {
             Bitmap bitmap = new Bitmap(new Uri(Image.Source.ToString()).LocalPath);
@@ -84,7 +84,7 @@ namespace ImageSlicer
             }
 
             //create link between two label
-            Label Merge2Label (Label label1, Label label2)
+            Label Merge2Label(Label label1, Label label2)
             {
                 if (label1 != label2)
                 {
@@ -100,14 +100,23 @@ namespace ImageSlicer
                 label.Status = 0;
                 if (lookUp[label.LabelNum].Count == 0)
                     return label;
-                
-                foreach(var num in lookUp[label.LabelNum])
+
+                foreach (var num in lookUp[label.LabelNum])
                 {
                     if (results[num].Status == 0)
                         continue;
                     Merge(label, SuperMerge(results[num]).Value);
                 }
                 return label;
+            }
+
+            //check target pixel
+            bool isTargetPixel(Color pixel)
+            {
+                // target condition
+                if (pixel.A != 0)
+                    return true;
+                return false;
             }
 
             using (var dialog = new FolderBrowserDialog())
@@ -117,34 +126,34 @@ namespace ImageSlicer
                 {
                     // -- Check first row of pixels
 
-                    if (bitmap.GetPixel(0, 0).A != 0) //check first pixel of row
+                    if (isTargetPixel(bitmap.GetPixel(0, 0))) //check first pixel of row
                     {
                         lookUp.Add(new HashSet<int>());
                         results.Add(new Label(results.Count, 1, new Pic(0, 0, 0, 0)));
                         last[0] = results.Last();
                     }
 
-                    for(int j = 0; j < bitmap.Width - 1; j++) //check the rest pixels of row
+                    for (int j = 0; j < bitmap.Width - 1; j++) //check the rest pixels of row
                     {
-                        if (bitmap.GetPixel(j, 0).A == 0 && bitmap.GetPixel(j + 1, 0).A != 0)
+                        if (!isTargetPixel(bitmap.GetPixel(j, 0)) && isTargetPixel(bitmap.GetPixel(j + 1, 0)))
                         {
                             lookUp.Add(new HashSet<int>());
                             results.Add(new Label(results.Count, 1, new Pic(j + 1, j + 1, 0, 0)));
                             last[j + 1] = results.Last();
                         }
-                        else if (bitmap.GetPixel(j, 0).A != 0 && bitmap.GetPixel(j + 1, 0).A != 0)
+                        else if (isTargetPixel(bitmap.GetPixel(j, 0)) && isTargetPixel(bitmap.GetPixel(j + 1, 0)))
                             last[j + 1] = Merge(last[j], new Pic(j + 1, j + 1, 0, 0));
                     }
 
                     // -- Check rows in middle
 
-                    for(int i = 1; i < bitmap.Height; i++)
+                    for (int i = 1; i < bitmap.Height; i++)
                     {
-                        if (bitmap.GetPixel(0, i).A != 0) //check first pixel of row
+                        if (isTargetPixel(bitmap.GetPixel(0, i))) //check first pixel of row
                         {
-                            if (bitmap.GetPixel(0, i - 1).A != 0) //check pixel above
+                            if (isTargetPixel(bitmap.GetPixel(0, i - 1))) //check pixel above
                                 current[0] = Merge(last[0], new Pic(0, 0, i, i));
-                            else if (bitmap.GetPixel(1, i - 1).A != 0) //check pixel above on right
+                            else if (isTargetPixel(bitmap.GetPixel(1, i - 1))) //check pixel above on right
                                 current[0] = Merge(last[1], new Pic(0, 0, i, i));
                             else // if arent any pixel above
                             {
@@ -156,21 +165,21 @@ namespace ImageSlicer
                         for (int j = 0; j < bitmap.Width - 2; j++) //check the rest pixels of row
                         {
                             //check firest appearance of non-transparent pixel on row
-                            if (bitmap.GetPixel(j, i).A == 0 && bitmap.GetPixel(j + 1, i).A != 0) 
+                            if (!isTargetPixel(bitmap.GetPixel(j, i)) && isTargetPixel(bitmap.GetPixel(j + 1, i)))
                             {
-                                if (bitmap.GetPixel(j + 1, i - 1).A != 0) //check pixel above
+                                if (isTargetPixel(bitmap.GetPixel(j + 1, i - 1))) //check pixel above
                                     current[j + 1] = Merge(last[j + 1], new Pic(j + 1, j + 1, i, i));
                                 else
                                 {
                                     bool check = true;
-                                    if (bitmap.GetPixel(j, i - 1).A != 0) //check pixel above on left
+                                    if (isTargetPixel(bitmap.GetPixel(j, i - 1))) //check pixel above on left
                                     {
                                         current[j + 1] = Merge(last[j], new Pic(j + 1, j + 1, i, i));
                                         check = false;
-                                        if (bitmap.GetPixel(j + 2, i - 1).A != 0) //check pixel above on right
+                                        if (isTargetPixel(bitmap.GetPixel(j + 2, i - 1))) //check pixel above on right
                                             current[j + 1] = Merge2Label(current[j + 1], last[j + 2]);
                                     }
-                                    else if (bitmap.GetPixel(j + 2, i - 1).A != 0) //check pixel above on right
+                                    else if (isTargetPixel(bitmap.GetPixel(j + 2, i - 1))) //check pixel above on right
                                     {
                                         current[j + 1] = Merge(last[j + 2], new Pic(j + 1, j + 1, i, i));
                                         check = false;
@@ -184,19 +193,19 @@ namespace ImageSlicer
                                 }
                             }
                             // check continuity of non-transparent pixel
-                            else if (bitmap.GetPixel(j, i).A != 0 && bitmap.GetPixel(j + 1, i).A != 0)
+                            else if (isTargetPixel(bitmap.GetPixel(j, i)) && isTargetPixel(bitmap.GetPixel(j + 1, i)))
                             {
-                                if (bitmap.GetPixel(j + 1, i - 1).A != 0) //check pixel above
+                                if (isTargetPixel(bitmap.GetPixel(j + 1, i - 1))) //check pixel above
                                     current[j + 1] = Merge2Label(Merge(current[j], new Pic(j + 1, j + 1, i, i)), last[j + 1]);
                                 else
                                 {
                                     bool check = true;
-                                    if (bitmap.GetPixel(j + 2, i - 1).A != 0) //check pixel above on right
+                                    if (isTargetPixel(bitmap.GetPixel(j + 2, i - 1))) //check pixel above on right
                                     {
                                         current[j + 1] = Merge2Label(Merge(current[j], new Pic(j + 1, j + 1, i, i)), last[j + 2]);
                                         check = false;
                                     }
-                                    if (bitmap.GetPixel(j, i - 1).A != 0) //check pixel above on left
+                                    if (isTargetPixel(bitmap.GetPixel(j, i - 1))) //check pixel above on left
                                     {
                                         current[j + 1] = Merge2Label(Merge(last[j], new Pic(j + 1, j + 1, i, i)), current[j]);
                                         check = false;
@@ -208,11 +217,11 @@ namespace ImageSlicer
                         }
                         // -- Check last row
                         int lastRow = bitmap.Width - 2;
-                        if (bitmap.GetPixel(lastRow, i).A == 0 && bitmap.GetPixel(lastRow + 1, i).A != 0)
+                        if (!isTargetPixel(bitmap.GetPixel(lastRow, i)) && isTargetPixel(bitmap.GetPixel(lastRow + 1, i)))
                         {
-                            if (bitmap.GetPixel(lastRow + 1, i - 1).A != 0)
+                            if (isTargetPixel(bitmap.GetPixel(lastRow + 1, i - 1)))
                                 current[lastRow + 1] = Merge(last[lastRow + 1], new Pic(lastRow + 1, lastRow + 1, i, i));
-                            else if (bitmap.GetPixel(lastRow, i - 1).A != 0)
+                            else if (isTargetPixel(bitmap.GetPixel(lastRow, i - 1)))
                                 current[lastRow + 1] = Merge(last[lastRow], new Pic(lastRow + 1, lastRow + 1, i, i));
                             else
                             {
@@ -221,11 +230,11 @@ namespace ImageSlicer
                                 current[lastRow + 1] = results.Last();
                             }
                         }
-                        else if (bitmap.GetPixel(lastRow, i).A != 0 && bitmap.GetPixel(lastRow + 1, i).A != 0)
+                        else if (isTargetPixel(bitmap.GetPixel(lastRow, i)) && isTargetPixel(bitmap.GetPixel(lastRow + 1, i)))
                         {
-                            if (bitmap.GetPixel(lastRow + 1, i - 1).A != 0)
+                            if (isTargetPixel(bitmap.GetPixel(lastRow + 1, i - 1)))
                                 current[lastRow + 1] = Merge2Label(Merge(current[lastRow], new Pic(lastRow + 1, lastRow + 1, i, i)), last[lastRow + 1]);
-                            else if (bitmap.GetPixel(lastRow, i - 1).A != 0)
+                            else if (isTargetPixel(bitmap.GetPixel(lastRow, i - 1)))
                                 current[lastRow + 1] = Merge2Label(Merge(current[lastRow], new Pic(lastRow + 1, lastRow + 1, i, i)), last[lastRow]);
                             else
                                 current[lastRow + 1] = Merge(current[lastRow], new Pic(lastRow + 1, lastRow + 1, i, i));
@@ -240,7 +249,7 @@ namespace ImageSlicer
 
                     //link all relative label
                     List<Label> finalResult = new List<Label>();
-                    for(int i = 0; i < results.Count; i++)
+                    for (int i = 0; i < results.Count; i++)
                     {
                         if (results[i].Status == 1)
                             finalResult.Add(SuperMerge(results[i]));
@@ -255,6 +264,9 @@ namespace ImageSlicer
                                                     img.Value.highY - img.Value.lowY + 1),
                                      bitmap.PixelFormat))
                         {
+                            // Image size condition
+                            //if (expImg.Width < 19 || expImg.Width > 70 || expImg.Height < 19 || expImg.Height > 70)
+                            //    continue;
                             expImg.Save($"{dialog.SelectedPath}\\{k}.png");
                             k++;
                         }
